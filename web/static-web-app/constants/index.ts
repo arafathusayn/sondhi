d12d291302743498f5312707851f64f84fe848c0
Arafat Husayn <hello@arafat.dev>
2022-11-17 15:57:07 +0600

    add explainer in the home page

--- web/static-web-app/constants/index.ts
+++ web/static-web-app/constants/index.ts
@@ -1,2 +1,4 @@
 export const COLOR_SCHEME_KEY_FOR_STORAGE = "mantine-color-scheme";
 export const SETTINGS_KEY_FOR_STORAGE = "lss-settings";
+export const siteName =
+  process.env.NEXT_PUBLIC_SITE_NAME || "Live Shared Secret";
--- web/static-web-app/pages/index.tsx
+++ web/static-web-app/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { Button, Stack } from "@mantine/core";
+import { Accordion, Button, List, Stack, Text } from "@mantine/core";
 import { PageLinks } from "../types";
 import {
   IconChartPie,
@@ -9,25 +9,18 @@ import {
   IconDoorEnter,
   IconHomeHeart,
 } from "@tabler/icons";
+import { siteName } from "../constants";
 
 const Home: NextPage = () => {
   return (
-    <Stack
-      align="center"
-      justify="center"
-      style={{
-        height: "70%",
-      }}
-      spacing="xl"
-    >
+    <Stack align="center" justify="center" spacing="xl">
       <Head>
-        <title>
-          {`${process.env.NEXT_PUBLIC_SITE_NAME || "Live Shared Secret"}`}
-        </title>
+        <title>{`${siteName}`}</title>
       </Head>
 
       <Link href={PageLinks.CreateShares} passHref>
         <Button
+          mt="xl"
           component="a"
           style={{ width: "80vw", maxWidth: "400px" }}
           variant="light"
@@ -101,6 +94,87 @@ const Home: NextPage = () => {
           Time-Lock
         </Button>
       </Link>
+
+      <Accordion
+        style={{ width: "80vw", maxWidth: "400px" }}
+        defaultValue="what"
+        styles={{
+          content: {
+            padding: "0px",
+          },
+        }}
+        pb="xl"
+      >
+        <Accordion.Item value="what" style={{ borderBottom: 0 }}>
+          <Accordion.Control style={{ paddingLeft: "0px" }}>
+            <Text size="md">What is {siteName}?</Text>
+          </Accordion.Control>
+          <Accordion.Panel
+            styles={{
+              root: {
+                margin: "0px",
+              },
+            }}
+          >
+            <Text size="sm">{`${
+              process.env.NEXT_PUBLIC_SITE_NAME || "Live Shared Secret"
+            } is a collection of tools that enables you
+            to protect and securely share your secrets using Shamir's Secret Sharing scheme
+            and an optional time-lock mechanism.`}</Text>
+
+            <Link href="https://youtu.be/ojMFCpUt7OU?t=262" target="_blank">
+              <Text size="sm" color="blue" mt="sm">
+                {`Watch the full explanation of Shamir's Secret Sharing and its implementation details on YouTube`}
+              </Text>
+            </Link>
+          </Accordion.Panel>
+        </Accordion.Item>
+
+        <Accordion.Item value="timelockhow" style={{ borderBottom: 0 }} pb="xl">
+          <Accordion.Control style={{ paddingLeft: "0px" }}>
+            <Text size="md" mt="sm">
+              How does the Time-Lock work?
+            </Text>
+          </Accordion.Control>
+          <Accordion.Panel
+            styles={{
+              root: {
+                margin: "0px",
+              },
+            }}
+            pb="xl"
+          >
+            <Text size="sm" mt="sm">
+              {`You'll provide two passwords:`}
+            </Text>
+
+            <List type="unordered" size="xs" spacing={0}>
+              <List.Item pt="xs" pb={0} mb={0}>
+                <Text size="sm">{`Admin Password`}</Text>
+              </List.Item>
+              <List.Item pt={0} mt={0}>
+                <Text size="sm">{`Recovery Password`}</Text>
+              </List.Item>
+            </List>
+
+            <Text size="sm" mt="sm">
+              {`The app will encrypt your admin password with your recovery password. The encrypted text of your admin password will be sent to the time-lock providers who will keep it locked for the time duration you mention.`}
+            </Text>
+
+            <Text size="sm" mt="sm">
+              {`You may split your recovery password into multiple shares using Shamir's secret sharing scheme. When the minimum shares are combined, the recovery password can be retrieved.`}
+            </Text>
+
+            <Text size="sm" mt="sm">
+              {`This recovery password can be used to request the unlock to the time-lock server. After the request is made, the time-lock server will wait until the lock duration is reached and then it will unlock and send the encrypted admin password back.`}
+            </Text>
+
+            <Text size="sm" mt="sm">
+              {`Using this process, you may use merge your admin password and recovery password to encrypt and time-lock your other secrets.`}
+            </Text>
+          </Accordion.Panel>
+        </Accordion.Item>
+      </Accordion>
     </Stack>
   );
 };
